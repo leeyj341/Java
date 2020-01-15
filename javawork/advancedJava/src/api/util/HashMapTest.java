57149024ace4a572b4c2b5f1d93ba8edf06dd4d8
leeyj341 <leeyj3794@gmail.com>
2020-01-15 09:26:34 +0900

    폴더 정리

--- javawork/advancedJava/src/api/util/HashMapTest.java
+++ javawork/advancedJava/src/api/util/HashMapTest.java
@@ -10,7 +10,7 @@ public class HashMapTest {
 		//	- Generic으로 데이터 타입을 명시할 때도 key에 대한 타입, 
 		//	  value에 대한 타입을 같이 정의해야 한다.
 		HashMap<Integer, String> map = new HashMap<Integer, String>();
-
+		map.put(1, "servlet");
 	}
 
 }
